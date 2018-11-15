1595f0cd22496857951a9bfde1165f2c5328490d
blmayer <bleemayer@gmail.com>
2018-11-15 18:42:04 -0200

    Added problem 6

--- 1.py
+++ 1.py
@@ -1,29 +1,9 @@
 """
-    This problem was recently asked by Google.
+    This problem was asked by Google.
 
-    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+    An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
 
-    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-
-    Bonus: Can you do this in one pass?
+    If using a language that has no pointers (such as Python), you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
 """
 
-# Get input
-array = input("Input list of number (space separated):\n")
-k = int(input("Input a number:\n"))
-
-# Tranform array into an array
-array = [int(x) for x in array.split(" ")]
-
 # Give expected return
-diffs = []
-for n in array:
-    if n in diffs:
-        print("True")
-        exit(0)
-    else:
-        diffs.append(k - n)
-
-print("False")
-exit(1)
-
